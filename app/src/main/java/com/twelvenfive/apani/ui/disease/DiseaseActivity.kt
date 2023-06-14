2fd0a6ec6fc6c9a97b9a9cf835d95c14e8f2048d
rfttra <auliafitra2002@gmail.com>
2023-06-14 11:02:17 +0700

    Plant Disease

--- app/src/main/java/com/twelvenfive/apani/ui/disease/DiseaseActivity.kt
+++ app/src/main/java/com/twelvenfive/apani/ui/disease/DiseaseActivity.kt
@@ -1,12 +1,24 @@
 package com.twelvenfive.apani.ui.disease
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.media.ThumbnailUtils
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import com.twelvenfive.apani.R
 import com.twelvenfive.apani.databinding.ActivityDiseaseBinding
+import com.twelvenfive.apani.ml.ModelPlantdisease
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import java.io.IOException
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 
 class DiseaseActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDiseaseBinding
+    private val imageSize = 224
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDiseaseBinding.inflate(layoutInflater)
@@ -15,5 +27,73 @@ class DiseaseActivity : AppCompatActivity() {
         binding.icBack.setOnClickListener {
             onBackPressed()
         }
+
+        binding.btnCheckDisease.setOnClickListener {
+            if (checkSelfPermission(android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+                val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(cameraIntent, 3)
+            }else{
+                requestPermissions(arrayOf(android.Manifest.permission.CAMERA), 100)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == RESULT_OK){
+            if (requestCode == 3){
+                val image = data?.extras?.get("data") as? Bitmap
+                val dimension = image?.width?.coerceAtMost(image.height)
+                val thumbnail = ThumbnailUtils.extractThumbnail(image, dimension ?: 0, dimension ?: 0)
+                binding.ivPreview.setImageBitmap(thumbnail)
+
+                val scaledImage = Bitmap.createScaledBitmap(thumbnail, imageSize,imageSize, false)
+                classifyImage(scaledImage)
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun classifyImage(image: Bitmap) {
+        try {
+            val model = ModelPlantdisease.newInstance(applicationContext)
+
+            val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.FLOAT32)
+            val byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3)
+            byteBuffer.order(ByteOrder.nativeOrder())
+
+            val intValues = IntArray(imageSize * imageSize)
+            image.getPixels(intValues, 0, image.width, 0, 0, image.width, image.height)
+            var pixel = 0
+            for (i in 0 until imageSize){
+                for (j in 0 until imageSize){
+                    val value = intValues[pixel++] //RGB
+                    byteBuffer.putFloat(((value shr 16) and 0xFF) * (1f / 255))
+                    byteBuffer.putFloat(((value shr 8) and 0xFF) * (1f / 255))
+                    byteBuffer.putFloat((value and 0xFF) * (1f / 255))
+                }
+            }
+
+            inputFeature0.loadBuffer(byteBuffer)
+
+            val outputs = model.process(inputFeature0)
+            val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+
+            val confidences = outputFeature0.floatArray
+            // Find the index of the class with the biggest confidence.
+            var maxPos = 0
+            var maxConfidence = 0f
+            for (i in confidences.indices) {
+                if (confidences[i] > maxConfidence) {
+                    maxConfidence = confidences[i]
+                    maxPos = i
+                }
+            }
+            val classes = arrayOf("Sehat", "Embun Tepung", "Karat Daun")
+            binding.tvDetectResult.text = classes[maxPos]
+
+        }catch (e : IOException){
+            //Handle exception
+        }
+
     }
 }
